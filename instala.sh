eb827f19cbf34dfec3e62b1c97feedbf6ada98e0
Cuenta Generica para Curso Intensivo Sobre Asimilacion de Datos y Teoria de Filtrado <adtf-02@ws22.labo2>
2019-10-07 17:51:38 -0300

    Minor update

--- instala.sh
+++ instala.sh
@@ -3,6 +3,8 @@ conda create --prefix /media/libre/daba
 
 source activate /media/libre/daba
 
+#conda info --envs
+
 pip install pot
 
 pip install tqdm
@@ -11,6 +13,6 @@ pip install plotly
 
 pip install spyder
 
-rm -fr .local/lib/
+#rm -fr .local/lib/
 
 
