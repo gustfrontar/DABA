7977d00ccae1f032c456926030902f6c6e6d47aa
jruiz <gustfrontar@gmail.com>
2024-01-29 15:33:28 -0300

    New experiments

--- Lorenz_96/experiments/Figures_Paper/FigureAnalRMSE_multinfyloc_Den1_Freq4_Hradar.py
+++ Lorenz_96/experiments/Figures_Paper/FigureAnalRMSE_multinfyloc_Den1_Freq4_Hradar.py
@@ -5,69 +5,100 @@ plt.rcParams['text.usetex'] = True
 
 plt.rcParams.update({'font.size': 22})
 
-NatureName='NatureR1_Den1_Freq4_Hradar'
-
-Sesitivity_experiment_multinfyloc_LETKF_NatureR1_Den1_Freq4_Hradar.npz
-exp_filename='../npz/Sensitivity_experiment_multinfyloc_LETKF_' + NatureName + '.npz'
+NatureName='NatureR03_Den1_Freq4_Hradar'
+exp_filename='../npz/Sesitivity_experiment_multinfyloc_LETKF_' + NatureName + '.npz'
 
 f=open(exp_filename,'rb')
-[results,gross_error_check_range, min_dbz_thresh_range,total_analysis_rmse,total_forecast_rmse,total_analysis_sprd,total_forecast_sprd] = pickle.load(f)
+[results,mult_inf_range,loc_scale_range,Alpha_temp_list,total_analysis_rmse,total_forecast_rmse,total_analysis_sprd,total_forecast_sprd] = pickle.load(f)
 f.close()
 
-NormalEnd = np.zeros( (len(gross_error_check_range) , len(min_dbz_thresh_range) ))
-for ii in range(len(gross_error_check_range)) :
-    for jj in range(len(min_dbz_thresh_range)) :
-        NormalEnd[ii,jj] = 1-results[ii*len(min_dbz_thresh_range)+jj]['NormalEnd']
+NormalEnd = np.zeros( (len(mult_inf_range) , len(loc_scale_range) ))
+for ii in range(len(mult_inf_range)) :
+    for jj in range(len(loc_scale_range)) :
+        NormalEnd[ii,jj] = 1-results[ii*len(loc_scale_range)+jj]['NormalEnd']
 NormalEnd=NormalEnd.astype(bool)    
         
 total_analysis_rmse[NormalEnd] = np.nan        
 total_analysis_sprd[NormalEnd] = np.nan      
 
-min_dbz_thresh_range = 1.0 - min_dbz_thresh_range
+fig , axs = plt.subplots( 4 , 1 , figsize=(7,24) )
 
-fig , axs = plt.subplots( 2 , 2 , figsize=(11,12) )
+pcolor0=axs[0].pcolor( loc_scale_range , mult_inf_range ,  total_analysis_rmse , vmin=0 , vmax=5.0 , cmap='YlGn' )
+plt.colorbar(pcolor0,ax=axs[0])
+axs[0].set_ylabel('Mult. Inf.')
 
-pcolor0=axs[0,0].pcolor( min_dbz_thresh_range, gross_error_check_range , total_analysis_rmse , vmin=0 , vmax=5.0 , cmap='YlGn' )
-plt.colorbar(pcolor0,ax=axs[0,0])
-axs[0,0].set_ylabel(r'$\beta$')
+axs[0].set_title('(a)')
 
-axs[0,0].set_title('(a)')
 
-pcolor1=axs[0,1].pcolor( min_dbz_thresh_range, gross_error_check_range , total_analysis_sprd , vmin=0 , vmax=5.0 , cmap='YlGn' )
-plt.colorbar(pcolor1,ax=axs[0,1])
-axs[0,1].set_title('(b)')
-
-min_error = np.round( np.min( total_analysis_rmse , axis = 0 ) , 2 )
-
-NatureName='NatureR5_Den1_Freq4_Hradar'
-
-exp_filename='../npz/Sesitivity_experiment_tempering_grosserr_LETKF_' + NatureName + '.npz'
+NatureName='NatureR1_Den1_Freq4_Hradar'
+exp_filename='../npz/Sesitivity_experiment_multinfyloc_LETKF_' + NatureName + '.npz'
 
 f=open(exp_filename,'rb')
-[results,gross_error_check_range, min_dbz_thresh_range,total_analysis_rmse,total_forecast_rmse,total_analysis_sprd,total_forecast_sprd] = pickle.load(f)
+[results,mult_inf_range,loc_scale_range,Alpha_temp_list,total_analysis_rmse,total_forecast_rmse,total_analysis_sprd,total_forecast_sprd] = pickle.load(f)
 f.close()
 
-NormalEnd = np.zeros( (len(gross_error_check_range) , len(min_dbz_thresh_range) ))
-for ii in range(len(gross_error_check_range)) :
-    for jj in range(len(min_dbz_thresh_range)) :
-        NormalEnd[ii,jj] = 1-results[ii*len(min_dbz_thresh_range)+jj]['NormalEnd']
-NormalEnd=NormalEnd.astype(bool) 
-
+NormalEnd = np.zeros( (len(mult_inf_range) , len(loc_scale_range) ))
+for ii in range(len(mult_inf_range)) :
+    for jj in range(len(loc_scale_range)) :
+        NormalEnd[ii,jj] = 1-results[ii*len(loc_scale_range)+jj]['NormalEnd']
+NormalEnd=NormalEnd.astype(bool)    
+        
 total_analysis_rmse[NormalEnd] = np.nan        
-total_analysis_sprd[NormalEnd] = np.nan   
+total_analysis_sprd[NormalEnd] = np.nan      
 
-min_dbz_thresh_range = 1.0 - min_dbz_thresh_range
+pcolor0=axs[1].pcolor( loc_scale_range , mult_inf_range ,  total_analysis_rmse , vmin=0 , vmax=5.0 , cmap='YlGn' )
+plt.colorbar(pcolor0,ax=axs[1])
+axs[1].set_ylabel('Mult. Inf.')
 
-pcolor2=axs[1,0].pcolor( min_dbz_thresh_range, gross_error_check_range , total_analysis_rmse , vmin=0 , vmax=5.0 , cmap='YlGn' )
-plt.colorbar(pcolor2,ax=axs[1,0])
-axs[1,0].set_xlabel(r'$T_{nr}$')
-axs[1,0].set_ylabel(r'$\beta$')
-axs[1,0].set_title('(c)')
-
-pcolor3=axs[1,1].pcolor( min_dbz_thresh_range, gross_error_check_range , total_analysis_sprd , vmin=0 , vmax=5.0 , cmap='YlGn' )
-plt.colorbar(pcolor3,ax=axs[1,1])
-axs[1,1].set_xlabel(r'$T_{nr}$')
-axs[1,1].set_title('(d)')
+axs[1].set_title('(b)')
 
 
-plt.savefig('FigureAnalRMSE_Grosserror_Multinf_R1_Den1_Freq4_Hradar.png')
+NatureName='NatureR5_Den1_Freq4_Hradar'
+exp_filename='../npz/Sesitivity_experiment_multinfyloc_LETKF_' + NatureName + '.npz'
+
+f=open(exp_filename,'rb')
+[results,mult_inf_range,loc_scale_range,Alpha_temp_list,total_analysis_rmse,total_forecast_rmse,total_analysis_sprd,total_forecast_sprd] = pickle.load(f)
+f.close()
+
+NormalEnd = np.zeros( (len(mult_inf_range) , len(loc_scale_range) ))
+for ii in range(len(mult_inf_range)) :
+    for jj in range(len(loc_scale_range)) :
+        NormalEnd[ii,jj] = 1-results[ii*len(loc_scale_range)+jj]['NormalEnd']
+NormalEnd=NormalEnd.astype(bool)    
+        
+total_analysis_rmse[NormalEnd] = np.nan        
+total_analysis_sprd[NormalEnd] = np.nan      
+
+pcolor0=axs[2].pcolor( loc_scale_range , mult_inf_range ,  total_analysis_rmse , vmin=0 , vmax=5.0 , cmap='YlGn' )
+plt.colorbar(pcolor0,ax=axs[2])
+axs[2].set_ylabel('Mult. Inf.')
+
+axs[2].set_title('(b)')
+
+
+NatureName='NatureR25_Den1_Freq4_Hradar'
+exp_filename='../npz/Sesitivity_experiment_multinfyloc_LETKF_' + NatureName + '.npz'
+
+f=open(exp_filename,'rb')
+[results,mult_inf_range,loc_scale_range,Alpha_temp_list,total_analysis_rmse,total_forecast_rmse,total_analysis_sprd,total_forecast_sprd] = pickle.load(f)
+f.close()
+
+NormalEnd = np.zeros( (len(mult_inf_range) , len(loc_scale_range) ))
+for ii in range(len(mult_inf_range)) :
+    for jj in range(len(loc_scale_range)) :
+        NormalEnd[ii,jj] = 1-results[ii*len(loc_scale_range)+jj]['NormalEnd']
+NormalEnd=NormalEnd.astype(bool)    
+        
+total_analysis_rmse[NormalEnd] = np.nan        
+total_analysis_sprd[NormalEnd] = np.nan      
+
+pcolor0=axs[3].pcolor( loc_scale_range , mult_inf_range ,  total_analysis_rmse , vmin=0 , vmax=5.0 , cmap='YlGn' )
+plt.colorbar(pcolor0,ax=axs[3])
+axs[3].set_ylabel('Mult. Inf.')
+
+axs[3].set_title('(b)')
+
+
+
+
+plt.savefig('FigureAnalRMSE_multinfyloc_Den1_Freq4_Hradar.png')
